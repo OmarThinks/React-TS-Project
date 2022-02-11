b637ec6db6efb6760b022715c414608defd85f96
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-11 19:13:07 +0200

    Wrote first TS Function

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -9,7 +9,35 @@ function App() {
   const helloWorld : string = "Hi";
   console.log(helloWorld);
   
-  
+
+  interface User {
+    name: string,
+    id: number
+  }
+
+
+
+  const user1 : User = {
+    name: "Hayes",
+    id: 0,
+  };
+  const user2 : User = {
+    name: "Hayes",
+    id: 0,
+  };
+
+  console.log(user1);
+  console.log(user2);
+  console.log(user1==user2);
+  console.log(user1===user2);
+
+
+function getUserName (user : User){
+  return user.name;
+}
+
+  console.log(getUserName(user1));
+
   
   return (
     <div className="App">
